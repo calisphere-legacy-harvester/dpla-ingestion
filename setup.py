99384e2c0ac43a94bbc219224d19c89f2403f940
Mark Redar <mredar@gmail.com>
2014-08-29 16:55:33 -0700

    bug fix, repeated version.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'dplaingestion',
-       version = '31.2',
        version = '32.0.0',
        description='DPLA Ingestion Subsystem',
        author='Digital Public Library of America',
