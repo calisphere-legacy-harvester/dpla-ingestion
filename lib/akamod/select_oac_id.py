c92042e3759602cd4d30f9017272169103f81fc6
Mark Redar <mredar@gmail.com>
2014-11-12 18:07:38 +0000

    Remove debug logger call

--- lib/akamod/select_oac_id.py
+++ lib/akamod/select_oac_id.py
@@ -5,7 +5,6 @@ from amara.lib.iri import is_absolute
 from akara.services import simple_service
 from akara.util import copy_headers_to_dict
 from akara import request, response
-from akara import logger
 from dplaingestion.selector import getprop, setprop, exists
 
 COUCH_ID_BUILDER = lambda src, lname: "--".join((src,lname))
@@ -28,7 +27,6 @@ def selectid(body, ctype):
     request_headers = copy_headers_to_dict(request.environ)
     source_name = request_headers.get('Source')
 
-    logger.error(str(data))
     objid = None
     v = getprop(data, 'identifier')
     if isinstance(v,basestring):
