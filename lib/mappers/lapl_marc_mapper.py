eb590ee7975b04cade8f72a211215b00b85c4915
Mark Redar <mredar@gmail.com>
2015-01-20 12:02:08 -0800

    Fix isShownAt to be computed from 910.
    Need to add fixed URL base to the the numerals from 910a.
    Stripping first 4 chars, which are CARL

--- lib/mappers/lapl_marc_mapper.py
+++ lib/mappers/lapl_marc_mapper.py
@@ -8,11 +8,34 @@ class LAPLMARCMapper(MARCMapper):
                 controlfield_tag='fields',
                 pymarc=True)
 
+        self.mapping_dict.update(
+                {lambda t: t == "856": [(self.map_is_shown_by, "u")]})
+        self.mapping_dict.update(
+                {lambda t: t == "910":  [(self.map_is_shown_at, "a")]})
+
     def map_data_provider(self):
         super(LAPLMARCMapper, self).map_data_provider(prop="collection")
 
 
-    def update_is_shown_at(self):
-        '''Cheat, use this to copy isShownAt to isShownBy for 
-        grabbing images'''
-        self.mapped_data['isShownBy'] = self.mapped_data['isShownAt']
+    def map_is_shown_at(self, _dict, tag, codes):
+        prop = "isShownAt"
+        values = self._get_values(_dict, codes)
+        if tag == "910":
+            print "VALUES:", str(values), ' TAG ', str(tag)
+            self.extend_prop(prop, _dict, codes, values=values)
+            if isinstance(self.mapped_data[prop], list):
+                # EDM says this is a single URL, not a list
+                self.mapped_data[prop] = self.mapped_data[prop][0]
+            self.mapped_data[prop] = ''.join((
+            'http://photos.lapl.org/carlweb/jsp/DoSearch?index=z&databaseID=968&count=10&initialsearch=true&terms=',
+            self.mapped_data[prop][4:]
+            ))
+
+    def map_is_shown_by(self, _dict, tag, codes):
+        prop = "isShownBy"
+        print "ISSHOWNBY CALLLED VALUES:"
+        self.extend_prop(prop, _dict, codes)
+        if isinstance(self.mapped_data[prop], list):
+            # EDM says this is a single URL, not a list
+            self.mapped_data[prop] = self.mapped_data[prop][0]
+
