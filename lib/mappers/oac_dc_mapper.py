df139e070998537a4cd0ed259a99eef62e3df90f
Mark Redar <mredar@gmail.com>
2016-10-14 16:11:42 -0700

    Flake 8 em

--- lib/mappers/oac_dc_mapper.py
+++ lib/mappers/oac_dc_mapper.py
@@ -6,20 +6,18 @@ from dplaingestion.utilities import iterify
 from akara import module_config
 
 URL_OAC_CONTENT_BASE = module_config().get(
-                        'url_oac_content',
-                        os.environ.get('URL_OAC_CONTENT_BASE',
-                                        'http://content.cdlib.org')
-                        )
+    'url_oac_content',
+    os.environ.get('URL_OAC_CONTENT_BASE', 'http://content.cdlib.org'))
 
 # collection 25496 has coverage values like A0800 & A1000
 # drop these
 Anum_re = re.compile('A\d\d\d\d')
 
+
 class OAC_DCMapper(DublinCoreMapper):
     '''Mapper for OAC xml feed objects'''
-    def get_values_from_text_attrib(self,
-                                    provider_prop,
-                                    suppress_attribs={}):
+
+    def get_values_from_text_attrib(self, provider_prop, suppress_attribs={}):
         '''Return a list of string values take from the OAC type
         original record (each value is {'text':<val>, 'attrib':<val>} object)
         '''
@@ -46,9 +44,9 @@ class OAC_DCMapper(DublinCoreMapper):
 
     # sourceResource mapping
     def source_resource_orig_to_prop(self,
-                                    provider_prop,
-                                    srcRes_prop,
-                                    suppress_attribs={}):
+                                     provider_prop,
+                                     srcRes_prop,
+                                     suppress_attribs={}):
         '''Override to handle elements which are dictionaries of format
         {'attrib': {}, 'text':"string value of element"}
         Args:
@@ -58,7 +56,7 @@ class OAC_DCMapper(DublinCoreMapper):
                 omit from mapping.
         '''
         values = self.get_values_from_text_attrib(provider_prop,
-                                             suppress_attribs)
+                                                  suppress_attribs)
         self.update_source_resource({srcRes_prop: values})
 
     def source_resource_prop_to_prop(self, prop, suppress_attribs={}):
@@ -67,11 +65,15 @@ class OAC_DCMapper(DublinCoreMapper):
         suppress_attribs is a dictionary of attribute key:value pairs to
         omit from mapping.
         '''
-        provider_prop = prop if not self.prefix else ''.join((self.prefix, prop))
-        self.source_resource_orig_to_prop(provider_prop, prop, suppress_attribs)
+        provider_prop = prop if not self.prefix else ''.join(
+            (self.prefix, prop))
+        self.source_resource_orig_to_prop(provider_prop, prop,
+                                          suppress_attribs)
 
-    def source_resource_orig_list_to_prop(
-            self, original_fields, srcRes_prop, suppress_attribs={}):
+    def source_resource_orig_list_to_prop(self,
+                                          original_fields,
+                                          srcRes_prop,
+                                          suppress_attribs={}):
         '''Override to handle elements which are dictionaries of format
         {'attrib': {}, 'text':"string value of element"}
         suppress_attribs is a dictionary of attribute key:value pairs to
@@ -83,23 +85,23 @@ class OAC_DCMapper(DublinCoreMapper):
         values = []
         for field in original_fields:
             if exists(self.provider_data_source, field):
-                values.extend(self.get_values_from_text_attrib(
-                    field,
-                    suppress_attribs))
+                values.extend(
+                    self.get_values_from_text_attrib(field, suppress_attribs))
         if values:
             self.update_source_resource({srcRes_prop: values})
 
     def get_best_oac_image(self):
         '''From the list of images, choose the largest one'''
         best_image = None
-        if self.provider_data.has_key('originalRecord'): # guard weird input
+        if 'originalRecord' in self.provider_data:  # guard weird input
             x = 0
             thumb = self.provider_data['originalRecord'].get('thumbnail', None)
             if thumb:
                 if 'src' in thumb:
                     x = thumb['X']
                     best_image = thumb['src']
-            ref_images = self.provider_data['originalRecord'].get('reference-image', [])
+            ref_images = self.provider_data['originalRecord'].get(
+                'reference-image', [])
             if type(ref_images) == dict:
                 ref_images = [ref_images]
             for obj in ref_images:
@@ -112,42 +114,55 @@ class OAC_DCMapper(DublinCoreMapper):
 
     def map_is_shown_at(self, index=None):
         ''' This is set in select-oac-id but must be added to mapped data'''
-        self.mapped_data.update({'isShownAt': self.provider_data.get('isShownAt', None)})
+        self.mapped_data.update({
+            'isShownAt': self.provider_data.get('isShownAt', None)
+        })
 
     def map_is_shown_by(self, index=None):
-        #already set in select_oac_id to base of {{obj url}}/thumbnail
+        # already set in select_oac_id to base of {{obj url}}/thumbnail
         best_image = self.get_best_oac_image()
         if best_image:
-            self.mapped_data.update( { "isShownBy" :  self.get_best_oac_image(),
-                })
+            self.mapped_data.update({"isShownBy": self.get_best_oac_image(), })
 
     def map_data_provider(self):
-        if self.provider_data.has_key('originalRecord'):
-            if self.provider_data['originalRecord'].has_key('collection'):
-                self.mapped_data.update({"dataProvider":
-                    self.provider_data['originalRecord']['collection']})
+        if 'originalRecord' in self.provider_data:  # guard weird input
+            if 'collection' in self.provider_data['originalRecord']:
+                self.mapped_data.update({
+                    "dataProvider":
+                    self.provider_data['originalRecord']['collection']
+                })
 
     def map_state_located_in(self):
-        self.update_source_resource({"stateLocatedIn": [{"name": "California"}]})
+        self.update_source_resource({
+            "stateLocatedIn": [{
+                "name": "California"
+            }]
+        })
 
     def map_spatial(self):
-        if self.provider_data.has_key('originalRecord'):
-            if self.provider_data['originalRecord'].has_key('coverage'):
-                coverage_data = iterify(getprop(self.provider_data['originalRecord'],
-                                                         "coverage"))
-                #remove arks from data
+        if 'originalRecord' in self.provider_data:  # guard weird input
+            if 'coverage' in self.provider_data['originalRecord']:
+                coverage_data = iterify(
+                    getprop(self.provider_data['originalRecord'], "coverage"))
+                # remove arks from data
                 # and move the "text" value to
-                coverage_data = [ c['text'] for c in coverage_data if (not isinstance(c,basestring) and not c['text'].startswith('ark:'))]
-                coverage_data = [ c for c in coverage_data if not Anum_re.match(c)]
-                self.update_source_resource({"spatial":coverage_data})
+                coverage_data = [
+                    c['text'] for c in coverage_data
+                    if (not isinstance(c, basestring) and not c['text']
+                        .startswith('ark:'))
+                ]
+                coverage_data = [
+                    c for c in coverage_data if not Anum_re.match(c)
+                ]
+                self.update_source_resource({"spatial": coverage_data})
 
     def map_format(self):
-        self.source_resource_prop_to_prop("format",
-                suppress_attribs={'q':'x'})
+        self.source_resource_prop_to_prop(
+            "format", suppress_attribs={'q': 'x'})
 
     def map_subject(self):
-        subject_values = self.get_values_from_text_attrib("subject",
-                suppress_attribs={'q':'series'})
+        subject_values = self.get_values_from_text_attrib(
+            "subject", suppress_attribs={'q': 'series'})
         subject_objs = [{'name': s} for s in subject_values]
         self.update_source_resource({"subject": subject_objs})
 
--- test/test_contentdm_oai_dc_mapper.py
+++ test/test_contentdm_oai_dc_mapper.py
@@ -10,16 +10,17 @@ DIR_FIXTURES = path.join(path.abspath(path.split(__file__)[0]), 'fixtures')
 #http://stackoverflow.com/questions/18084476/is-there-a-way-to-use-python-unit-test-assertions-outside-of-a-testcase
 TC = TestCase('__init__')
 
+
 def _get_server_response(body):
     url = server() + "dpla_mapper?mapper_type=contentdm_oai_dc"
     return H.request(url, "POST", body=body)
+
 
 @attr(uses_network='yes')
 def test_contentdm_oai_dc_mapping():
     # at this point, the ucsd feed should be "jsonfied"
     # need to map from the jsonfied obj to sourceResource
-    fixture = path.join(DIR_FIXTURES,
-            'contentdm_oai_dc_only.json')
+    fixture = path.join(DIR_FIXTURES, 'contentdm_oai_dc_only.json')
     with open(fixture) as f:
         INPUT = f.read()
         TC.assertIn('id', INPUT)
@@ -30,54 +31,68 @@ def test_contentdm_oai_dc_mapping():
     TC.assertIn('originalRecord', obj)
     srcRes = obj['sourceResource']
     TC.assertEqual(srcRes['title'],
-            ["Letter from J. D. Black to H. R. Coffin"])
+                   ["Letter from J. D. Black to H. R. Coffin"])
     TC.assertEqual(srcRes['date'], [u'1924-07-24', u'1920-1929'])
     TC.assertEqual(srcRes['contributor'],
-        [u'Support provided by the Metabolic Studio.'])
+                   [u'Support provided by the Metabolic Studio.'])
     TC.assertEqual(srcRes['creator'], [u'Coffin, H. R.'])
-    TC.assertEqual(srcRes['subject'], [
-        {u'name': u'Business'},
-        {u'name': u'Black, J. D. (John David), 1893-1960'},
-        {u'name': u'Coffin, H. R.'},
-        {u'name': u'Water rights--California--Owens Valley'},
-        {u'name': u'Water rights--California--Los Angeles'},
-        {u'name': u'Metabolic Studio'},
-        {u'name': u'Los Angeles Aqueduct'},
-        {u'name': u'LA Aqueduct'},
-        {u'name': u'Acquaduct'}])
+    TC.assertEqual(srcRes['subject'], [{
+        u'name': u'Business'
+    }, {
+        u'name': u'Black, J. D. (John David), 1893-1960'
+    }, {
+        u'name': u'Coffin, H. R.'
+    }, {
+        u'name': u'Water rights--California--Owens Valley'
+    }, {
+        u'name': u'Water rights--California--Los Angeles'
+    }, {
+        u'name': u'Metabolic Studio'
+    }, {
+        u'name': u'Los Angeles Aqueduct'
+    }, {
+        u'name': u'LA Aqueduct'
+    }, {
+        u'name': u'Acquaduct'
+    }])
 
-    TC.assertEqual(srcRes['spatial'], [u'Los Angeles (Calif.)',
-                                       u'Big Pine (Calif.)'])
+    TC.assertEqual(srcRes['spatial'],
+                   [u'Los Angeles (Calif.)', u'Big Pine (Calif.)'])
     TC.assertEqual(srcRes['relation'], [
-                            u'J. D. Black Papers',
-                            u'CSLA-15',
-                            u'Series 1.; Box No. 8; Folder No. 4'])
+        u'J. D. Black Papers', u'CSLA-15',
+        u'Series 1.; Box No. 8; Folder No. 4'
+    ])
     TC.assertEqual(srcRes['description'], [
-      "Letter from J. D. Black in Big Pine to H. R. Coffin",
-      "J. D. Black (1893-1960), also known as Jack"])
-    TC.assertEqual(srcRes['type'], [ "Letters", "Text"])
+        "Letter from J. D. Black in Big Pine to H. R. Coffin",
+        "J. D. Black (1893-1960), also known as Jack"
+    ])
+    TC.assertEqual(srcRes['type'], ["Letters", "Text"])
     TC.assertEqual(srcRes['language'], ['eng'])
     TC.assertEqual(srcRes['rights'], [
-        u'http://library.lmu.edu/generalinformation/departments/digitallibraryprogram/copyrightandreproductionpolicy/'])
+        u'http://library.lmu.edu/generalinformation/departments/digitallibraryprogram/copyrightandreproductionpolicy/'
+    ])
     TC.assertEqual(srcRes['format'], ["2 pages", "image/tiff"])
     TC.assertEqual(srcRes['identifier'], [
-      'http://archive.org/details/cubanc_000177',
-      "sc_jdbp001110001",
-      "sc_jdbp00111",
-      "http://digitalcollections.lmu.edu/cdm/ref/collection/johndblack/id/262",
-      "http://cdm15972.contentdm.oclc.org/bogus"
+        'http://archive.org/details/cubanc_000177', "sc_jdbp001110001",
+        "sc_jdbp00111",
+        "http://digitalcollections.lmu.edu/cdm/ref/collection/johndblack/id/262",
+        "http://cdm15972.contentdm.oclc.org/bogus"
     ])
-    TC.assertEqual(obj['isShownAt'],
-      "http://digitalcollections.lmu.edu/cdm/ref/collection/johndblack/id/262")
-    TC.assertEqual(obj['isShownBy'],
-      "http://digitalcollections.lmu.edu/utils/getthumbnail/collection/johndblack/id/262")
+    TC.assertEqual(
+        obj['isShownAt'],
+        "http://digitalcollections.lmu.edu/cdm/ref/collection/johndblack/id/262"
+    )
+    TC.assertEqual(
+        obj['isShownBy'],
+        "http://digitalcollections.lmu.edu/utils/getthumbnail/collection/johndblack/id/262"
+    )
+
 
 def test_suppress_sound_thumbs():
     '''For type "sound", remove the isShownBy value so no image will be
     harvested.
     '''
-    fixture = path.join(DIR_FIXTURES,
-            'cavpp_contentdm_oai.json')
+    fixture = path.join(DIR_FIXTURES, 'cavpp_contentdm_oai.json')
     with open(fixture) as f:
         INPUT = f.read()
         TC.assertIn('id', INPUT)
@@ -89,14 +104,14 @@ def test_suppress_sound_thumbs():
     TC.assertEqual(obj['sourceResource']['type'], ['Sound'])
     TC.assertNotIn('isShownBy', obj)
 
+
 @attr(uses_network='yes')
 def test_get_larger_image():
     '''For type "image", there is usually (always?) a larger image available.
     This should only be tested when network available, couldn't get httpretty
     to work in this context.
     '''
-    fixture = path.join(DIR_FIXTURES,
-            'contentdm_oai_image_type.json')
+    fixture = path.join(DIR_FIXTURES, 'contentdm_oai_image_type.json')
     with open(fixture) as f:
         INPUT = f.read()
         TC.assertIn('id', INPUT)
@@ -106,5 +121,7 @@ def test_get_larger_image():
     TC.assertIn('sourceResource', obj)
     TC.assertIn('originalRecord', obj)
     TC.assertEqual(obj['sourceResource']['type'], ['Still Image'])
-    TC.assertEqual(obj['isShownBy'],
-            "http://cdm16745.contentdm.oclc.org/utils/ajaxhelper?CISOROOT=brubeckcollection&CISOPTR=103&action=2&DMHEIGHT=2000&DMWIDTH=2000&DMSCALE=66")
+    TC.assertEqual(
+        obj['isShownBy'],
+        "http://cdm16745.contentdm.oclc.org/utils/ajaxhelper?CISOROOT=brubeckcollection&CISOPTR=103&action=2&DMHEIGHT=2000&DMWIDTH=2000&DMSCALE=66"
+    )
