1aa211a1ae0c5efc8aefdb7c95c3f6e12ac2c740
amywieliczka <amywieliczka@gmail.com>
2020-08-26 14:07:25 -0700

    theoretical stripping base mapper class out of ucldc_nuxeo_mapper

--- lib/mappers/ucldc_nuxeo_mapper.py
+++ lib/mappers/ucldc_nuxeo_mapper.py
@@ -1,46 +1,108 @@
-from dplaingestion.mappers.mapper import Mapper
 from dplaingestion.selector import exists
 from jsonpath import jsonpath
 from akara import logger
 
 
 #TODO: get from akara.ini?
-url_nuxeo_base = 'https://nuxeo.cdlib.org/Nuxeo/nxpicsfile/default/'
-
-url_nuxeo_pic_template_med_sz = ''.join((url_nuxeo_base, '{}/Medium:content/'))
+# url_nuxeo_base = 'https://nuxeo.cdlib.org/Nuxeo/nxpicsfile/default/'
+# url_nuxeo_pic_template_med_sz = ''.join((url_nuxeo_base, '{}/Medium:content/'))
 
 url_calisphere_item_base = 'https://calisphere.org/item/'
 
-class UCLDCNuxeoMapper(Mapper):
+class UCLDCNuxeoMapper(object):
 
     def __init__(self, provider_data):
-        super(UCLDCNuxeoMapper, self).__init__(provider_data)
+        self.provider_data = provider_data
+        self.mapped_data = {"sourceResource": {}}
         self.provider_data_source = jsonpath(self.provider_data, '$.properties')[0]
 
-    def map_source_resource(self):
-        super(UCLDCNuxeoMapper, self).map_source_resource()
-        self.map_alt_title()
-        self.map_genre()
-        self.map_provenance()
+        # type of mapping - helper functions
+   def array_wrap_map(source, dest, dest_field):
+        if exists(self.provider_data_source, source):
+            arr = [self.provider_data_source.get(source)]
+            self.mapped_data[dest].update({dest_field: arr})
 
-    def map_original_record(self):
-        super(UCLDCNuxeoMapper, self).map_original_record()
-        if not self.mapped_data['originalRecord']: #during testing, this is None
-            self.mapped_data['originalRecord'] = {}
-        self.map_source()
-        self.map_location()
+    def straight_map_provider_data(source, dest_field):
+        if exists(self.provider_data, source):
+            self.mapped_data.update({dest_field: self.provider_data.get(source)})
+
+    def straight_map(source, dest, dest_field):
+        if exists(self.provider_data_source, source):
+            self.mapped_data[dest].update({dest_field: self.provider_data_source.get(source)})
+
+    def aggregate_subfield_map(source, src_subfield, dest, dest_field):
+        if exists(self.provider_data_source, source):
+            aggregate = [a[src_subfield] for a in self.provider_data_source.get(source)]
+            self.mapped_data[dest].update({dest_field: aggregate})
+    
+    def force_array_map(source, dest, dest_field):
+        if exists(self.provider_data_source, source):
+            arr = [a for a in self.provider_data_source.get(source)]
+            self.mapped_data[dest].update({dest_field: arr})
+
+
+    def map(self):
+        """
+        Maps fields from provider_data to fields in mapped_data by:
+
+        1. Mapping, one-to-one, provider_data fields to mapped_data root fields
+           via map_root
+        2. Mapping, one-to-one, provider_data fields to mapped_data
+           sourceResource fields via map_source_resource
+        3. Mapping, one-to-many or many-to-one, provider_data fields to
+           mapped_data fields
+        4. Running any post-mapping logic via update_mapped_fields
+        """
+        self.map_root()
+        self.map_source_resource()
+        self.update_mapped_fields()
+
+    """Maps the mapped_data root fields."""
+    def map_root(self):
+        """Maps base fields shared by all Mappers"""
+        self.mapped_data["originalRecord"] = self.provider_data.get("originalRecord", {})
+        self.straight_map('ucldc_schema:source', 'originalRecord', 'source')
+        self.straight_map('ucldc_schema:physlocation', 'originalRecord', 'location')
         self.map_rights_holder()
         self.map_rights_note()
-        self.map_copyrighted()
-        self.map_transcription()
+        self.straight_map('ucldc_schema:rightsstartdate', 'originalRecord', 'dateCopyrighted')
+        self.straight_map('ucldc_schema:transcription', 'originalRecord', 'transcription')
 
-    def update_original_record(self, _dict):
-        """
-        Updates the mapped_data originalRecord field with the given dictionary.
-        """
-        self.mapped_data["originalRecord"].update(_dict)
+        id = self.provider_data.get("id", "")
+        _id = self.provider_data.get("_id")
+        at_id = "http://ucldc.cdlib.org/api/items/" + id
+        self.mapped_data.update({"id": id, "_id": _id, "@id": at_id})
 
-    # root mapping
+        for prop in ("ingestDate", "ingestType", "ingestionSequence"):
+            self.mapped_data.update({prop: self.provider_data.get(prop)})
+
+        # map_provider
+        self.straight_map_provider_data('provider', 'provider')
+        # map_data_provider
+        self.straight_map_provider_data('source', 'dataProvider')
+        self.map_is_shown_at()
+
+    # originalRecord mapping
+    def map_rights_holder(self):
+        rightsholders = []
+        if exists(self.provider_data_source, 'ucldc_schema:rightsholder'):
+            rightsholders = [rh['name'] for rh in self.provider_data_source.get('ucldc_schema:rightsholder')]
+        if exists(self.provider_data_source, 'ucldc_schema:rightscontact'):
+            rightsholders.append(self.provider_data_source.get('ucldc_schema:rightscontact'))
+        if rightsholders:
+            self.mapped_data["originalRecord"].update({'rightsHolder': rightsholders})
+
+    def map_rights_note(self):
+        rightsnotes = []
+        if exists(self.provider_data_source, 'ucldc_schema:rightsnotice'):
+            rightsnotes.append(self.provider_data_source.get('ucldc_schema:rightsnotice'))
+        if exists(self.provider_data_source, 'ucldc_schema:rightsnote'):
+            rightsnotes.append(self.provider_data_source.get('ucldc_schema:rightsnote'))
+        if rightsnotes:
+            self.mapped_data["originalRecord"].update({'rightsNote': rightsnotes})
+
+        # root mapping
+
     def map_is_shown_at(self):
         # these live on calisphere. so the isShownAt is:
         # https://calisphere.org/item/<ID>
@@ -50,28 +112,32 @@ class UCLDCNuxeoMapper(Mapper):
         self.mapped_data.update({"isShownBy":
                                   self.provider_data.get('isShownBy')})
 
+    def map_source_resource(self):
+        """Mapps the mapped_data sourceResource fields."""
+        aggregate_subfield_map('ucldc_schema:contributor', 'name', 'sourceResource', 'contributor')
+        aggregate_subfield_map('ucldc_schema:creator', 'name', 'sourceResource', 'creator')
+        aggregate_subfield_map('ucldc_schema:date', 'date', 'sourceResource', 'date')
+        self.map_description()          #pass
+        straight_map('ucldc_schema:extent', 'sourceResource', 'extent')
+        straight_map('ucldc_schema:physdesc', 'sourceResource', 'format')
+        self.map_identifier()           #pass
+        self.map_is_part_of()           #pass
+        self.map_language()             #pass
+        force_array_map('ucldc_schema:publisher', 'sourceResource', 'publisher')
+        self.map_relation()             #pass
+        self.map_rights()               #pass
+        self.map_spatial()              #pass
+        # self.map_spec_type()            #pass
+        # self.map_state_located_in()     #pass
+        self.map_subject()              #pass
+        force_array_map('ucldc_schema:temporalcoverage', 'sourceResource', 'temporalCoverage')
+        array_wrap_map('dc:title', 'sourceResource', 'title')
+        straight_map('ucldc_schema:type', 'sourceResource', 'type')
+        aggregate_subfield_map('ucldc_schema:formgenre', 'heading', 'sourceResource', 'genre')
+        straight_map('ucldc_schema:provenance', 'sourceResource', 'provenance')
+        force_array_map('ucldc_schema:alternativetitle', 'sourceResource', 'alternativeTitle')
+
     # sourceResource mapping
-    def map_contributor(self):
-        if exists(self.provider_data_source, 'ucldc_schema:contributor'):
-            contributors = [contrib['name'] for contrib in self.provider_data_source.get('ucldc_schema:contributor')]
-            self.update_source_resource({'contributor': contributors})
-
-    def map_alt_title(self):
-        if exists(self.provider_data_source, 'ucldc_schema:alternativetitle'):
-            alt_titles = [at for at in self.provider_data_source.get('ucldc_schema:alternativetitle')]
-            self.update_source_resource({'alternativeTitle': alt_titles})
-
-    def map_creator(self):
-        if exists(self.provider_data_source, 'ucldc_schema:creator'):
-            names = [creator['name'] for creator in self.provider_data_source.get('ucldc_schema:creator')]
-            self.update_source_resource({'creator': names})
-
-
-    def map_date(self):
-        if exists(self.provider_data_source, 'ucldc_schema:date'):
-            dates = [date['date'] for date in self.provider_data_source.get('ucldc_schema:date')]
-            self.update_source_resource({'date': dates})
-
     type_labels = {
             'scopecontent': 'Scope/Content',
             'acquisition': 'Acquisition',
@@ -124,20 +190,7 @@ class UCLDCNuxeoMapper(Mapper):
                     desc_data.append(self.unpack_description_data(d))
             else:
                 desc_data.append(self.unpack_description_data(raw_data))
-            self.update_source_resource({'description':desc_data})
-
-    def map_extent(self):
-        if exists(self.provider_data_source, 'ucldc_schema:extent'):
-            self.update_source_resource({'extent':self.provider_data_source.get('ucldc_schema:extent')})
-
-    def map_format(self):
-        if exists(self.provider_data_source, 'ucldc_schema:physdesc'):
-            self.update_source_resource({'format':self.provider_data_source.get('ucldc_schema:physdesc')})
-
-    def map_genre(self):
-        if exists(self.provider_data_source, 'ucldc_schema:formgenre'):
-            genres = [fg['heading'] for fg in self.provider_data_source.get('ucldc_schema:formgenre')]
-            self.update_source_resource({'genre': genres})
+            self.mapped_data["sourceResource"].update({'description':desc_data})
 
     def map_identifier(self):
         identifiers = []
@@ -147,7 +200,7 @@ class UCLDCNuxeoMapper(Mapper):
             localids = self.provider_data_source.get('ucldc_schema:localidentifier')
             identifiers.extend(localids)
         if identifiers:
-            self.update_source_resource({'identifier': identifiers})
+            self.mapped_data["sourceResource"].update({'identifier': identifiers})
 
     def map_language(self):
         languages = []
@@ -157,19 +210,14 @@ class UCLDCNuxeoMapper(Mapper):
                     languages.append(lang['language'])
                 if lang['languagecode']:
                     languages.append(lang['languagecode'])
-        self.update_source_resource({'language': [{'iso639_3': l} for l in languages]})
-
-    def map_publisher(self):
-        if exists(self.provider_data_source, 'ucldc_schema:publisher'):
-            publishers = [pub for pub in self.provider_data_source.get('ucldc_schema:publisher')]
-            self.update_source_resource({'publisher': publishers})
+        self.mapped_data["sourceResource"].update({'language': [{'iso639_3': l} for l in languages]})
 
     def map_relation(self):
         relations = []
         if exists(self.provider_data_source, 'ucldc_schema:relatedresource'):
             relations = [rr for rr in self.provider_data_source.get('ucldc_schema:relatedresource')]
         if relations:
-            self.update_source_resource({'relation': relations})
+            self.mapped_data["sourceResource"].update({'relation': relations})
 
     def map_rights_codes(self, rights_str):
         '''Map the "coded" values of the rights status to a nice one for
@@ -191,7 +239,7 @@ class UCLDCNuxeoMapper(Mapper):
             rights.append(self.map_rights_codes(rights_status))
         if exists(self.provider_data_source, 'ucldc_schema:rightsstatement'):
             rights.append(self.provider_data_source.get('ucldc_schema:rightsstatement'))
-        self.update_source_resource({'rights': rights})
+        self.mapped_data["sourceResource"].update({'rights': rights})
 
     def map_spatial(self):
         spatial = []
@@ -202,62 +250,10 @@ class UCLDCNuxeoMapper(Mapper):
                 if place['coordinates']:
                     spatial.append(place['coordinates'])
         if spatial:
-            self.update_source_resource({'spatial': [{'text': s} for s in spatial]})
+            self.mapped_data["sourceResource"].update({'spatial': [{'text': s} for s in spatial]})
 
     def map_subject(self):
         if exists(self.provider_data_source, 'ucldc_schema:subjecttopic'):
             subjects = [st['heading'] for st in self.provider_data_source.get('ucldc_schema:subjecttopic')]
             subjects.extend([sn['name'] for sn in self.provider_data_source.get('ucldc_schema:subjectname')])
-            self.update_source_resource({'subject': subjects})
-
-    def map_temporal(self):
-        if exists(self.provider_data_source, 'ucldc_schema:temporalcoverage'):
-            self.update_source_resource({'temporalCoverage': [tc for tc in self.provider_data_source.get('ucldc_schema:temporalcoverage')]})
-
-    def map_title(self):
-        if exists(self.provider_data_source, 'dc:title'):
-            titles = [self.provider_data_source.get('dc:title')]
-            self.update_source_resource({'title': titles})
-
-    def map_type(self):
-        if exists(self.provider_data_source, 'ucldc_schema:type'):
-            self.update_source_resource({'type': self.provider_data_source.get('ucldc_schema:type')})
-
-    # originalRecord mapping
-    def map_source(self):
-        if exists(self.provider_data_source, 'ucldc_schema:source'):
-            self.update_original_record({'source': self.provider_data_source.get('ucldc_schema:source')})
-
-    def map_provenance(self):
-        if exists(self.provider_data_source, 'ucldc_schema:provenance'):
-            self.update_source_resource({'provenance': self.provider_data_source.get('ucldc_schema:provenance')})
-
-    def map_location(self):
-        if exists(self.provider_data_source, 'ucldc_schema:physlocation'):
-            self.update_original_record({'location': self.provider_data_source.get('ucldc_schema:physlocation')})
-
-    def map_rights_holder(self):
-        rightsholders = []
-        if exists(self.provider_data_source, 'ucldc_schema:rightsholder'):
-            rightsholders = [rh['name'] for rh in self.provider_data_source.get('ucldc_schema:rightsholder')]
-        if exists(self.provider_data_source, 'ucldc_schema:rightscontact'):
-            rightsholders.append(self.provider_data_source.get('ucldc_schema:rightscontact'))
-        if rightsholders:
-            self.update_original_record({'rightsHolder': rightsholders})
-
-    def map_rights_note(self):
-        rightsnotes = []
-        if exists(self.provider_data_source, 'ucldc_schema:rightsnotice'):
-            rightsnotes.append(self.provider_data_source.get('ucldc_schema:rightsnotice'))
-        if exists(self.provider_data_source, 'ucldc_schema:rightsnote'):
-            rightsnotes.append(self.provider_data_source.get('ucldc_schema:rightsnote'))
-        if rightsnotes:
-            self.update_original_record({'rightsNote': rightsnotes})
-
-    def map_copyrighted(self):
-        if exists(self.provider_data_source, 'ucldc_schema:rightsstartdate'):
-           self.update_original_record({'dateCopyrighted': self.provider_data_source.get('ucldc_schema:rightsstartdate')})
-
-    def map_transcription(self):
-        if exists(self.provider_data_source, 'ucldc_schema:transcription'):
-          self.update_original_record({'transcription': self.provider_data_source.get('ucldc_schema:transcription')})
+            self.mapped_data["sourceResource"].update({'subject': subjects})
