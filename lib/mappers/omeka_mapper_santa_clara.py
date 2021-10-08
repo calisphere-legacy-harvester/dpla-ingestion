dc8a4384ff1ca0403339970e7f43db30b03b5ab1
Barbara Hui <barbara.hui@ucop.edu>
2021-10-08 15:37:44 -0700

    Remove __init__ from Santa Clara mapper

--- lib/mappers/omeka_mapper_santa_clara.py
+++ lib/mappers/omeka_mapper_santa_clara.py
@@ -5,9 +5,6 @@ import requests
 
 class Omeka_OAI_SantaClara_mapper(Omeka_OAIMapper):
 
-    def __init__(self, provider_data):
-        super(Omeka_OAI_SantaClara_mapper, self).__init__(provider_data)
-
     def map_is_shown_at(self):
         isShownAt = None
         idents = getprop(self.provider_data_source, 'identifier')
