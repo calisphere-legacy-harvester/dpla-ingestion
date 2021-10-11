99940f55da65906881fb9ef653be4b6659c0b075
Barbara Hui <barbara.hui@ucop.edu>
2021-10-11 12:44:37 -0700

    Rights mapper for Santa Clara County

--- lib/mappers/omeka_santa_clara_mapper.py
+++ lib/mappers/omeka_santa_clara_mapper.py
@@ -11,10 +11,26 @@ class Omeka_OAI_SantaClara_mapper(Omeka_OAIMapper):
     def map_is_shown_at(self):
         isShownAt = None
         idents = getprop(self.provider_data_source, 'identifier')
-        print("idents: {}".format(idents))
         for i in filter(None, idents):
             if i.startswith('https://sccbosarchive.org/api/items/'):
                 omeka_id = i.split('/')[-1]
                 isShownAt = 'https://sccbosarchive.org/s/home/item/{}'.format(omeka_id)
         if isShownAt:
             self.mapped_data.update({'isShownAt': isShownAt})
+
+    '''
+        rights value in fetched metadata is formatted like this:
+        ["No Copyright: http://rightsstatements.org/vocab/NoC-US/1.0/"]
+        strip out everything but the URI
+    '''
+    def map_rights(self):
+        rights_list = []
+        source_rights = getprop(self.provider_data_source, 'rights')
+        for r in filter(None, source_rights):
+            if 'rightsstatements.org' in r:
+                rights = r.split(': ')[1]
+                rights_list.append(rights)
+            else:
+                rights_list.append(r)
+        if rights_list:
+            self.update_source_resource({'rights': rights_list})
